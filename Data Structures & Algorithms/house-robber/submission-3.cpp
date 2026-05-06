ed915d8f6fa527fe48f8a8b93e0dd09ef5ea4a5b
Udaykumar Dhokia <udaykumardhokia@gmail.com>
2026-05-06 12:19:16 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev2 = 0;
+        int prev1 = nums[0];
+
+        for(int i=1; i<n; i++){
+            int pick = nums[i];
+            if(i > 1) pick += prev2;
+            int notPick = 0 + prev1;
+            int curr = max(pick, notPick);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
